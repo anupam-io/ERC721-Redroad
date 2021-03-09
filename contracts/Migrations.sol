a3701b7a596a9e6909ac018529c87e9017823171
akcgjc007 <akcgjc007@gmail.com>
2021-03-09 16:29:43 +0530

    Initial commit

---
+++ contracts/Migrations.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity <=0.8.0;
+
+contract Migrations {
+  address public owner = msg.sender;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+}
