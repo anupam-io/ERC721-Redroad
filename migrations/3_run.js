95297e46894e7856e221aae640bf2e12a44d9d33
akcgjc007 <akcgjc007@gmail.com>
2021-03-29 00:30:10 +0530

    changed main to master in uri

--- migrations/3_run.js
+++ migrations/3_run.js
@@ -2,7 +2,7 @@ const Redroad = artifacts.require("Redroad");
 
 module.exports = async function (deployer) {
     const road = await Redroad.deployed();
-    const url = `https://raw.githubusercontent.com/akcgjc007/erc721-Redroad/main/meta/0.json`;
+    const url = `https://raw.githubusercontent.com/akcgjc007/erc721-Redroad/master/meta/0.json`;
     
     const account = (await web3.eth.getAccounts())[0];
     
